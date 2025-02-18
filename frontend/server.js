28e120469032240e44404d4e66a8851666c7d12a
Kieran Llarena <baymax709@gmail.com>
2025-02-18 15:26:21 -0500

    refactor: rm express fetching to flask

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const path = require('path');
 const cors = require('cors');
 require('dotenv/config');
@@ -15,28 +14,11 @@ server.use(express.json());
 
 server.use(cors());
 
-server.get('/', (req, res) => {
+//Serve static files from 'templates' directory
+server.use(express.static(path.join(__dirname,'/static')));
+
+server.get('/', (_, res) => {
     res.sendFile(path.join(__dirname, 'templates', 'index.html'));
-});
-
-server.post('/', async (req, res) => {
-    const {password} = req.body;
-    console.error({password})
-    try {
-        const response = await fetch('http://localhost:5000/', {
-            method: 'POST', //change to GET for bugs
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({password})
-        });
-
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error('Error communicating with Flask server:', error.message);
-        res.status(500).json({success: false, message: 'Server error'});
-    }
 });
 
 server.listen(port, (error) => {
